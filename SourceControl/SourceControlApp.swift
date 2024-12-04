4f97918654d4394b176243643cd800aa535ad4a5
Eric <coyotleric@gmail.com>
2024-12-04 13:24:39 -0800

    Initial Commit

---
+++ SourceControl/SourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlApp.swift
+//  SourceControl
+//
+//  Created by Eric Coyotl on 12/4/24.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SourceControlTests/SourceControlTests.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlTests.swift
+//  SourceControlTests
+//
+//  Created by Eric Coyotl on 12/4/24.
+//
+
+import Testing
+@testable import SourceControl
+
+struct SourceControlTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
