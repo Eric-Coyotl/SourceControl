41b0c31789eca44ccb150d69529a8b40881595a4
Eric <coyotleric@gmail.com>
2024-12-04 18:40:22 -0800

    HomeView updates progress

--- SourceControl/HomeView.swift
+++ SourceControl/HomeView.swift
@@ -10,7 +10,15 @@ import SwiftUI
 struct HomeView: View {
     var body: some View {
         Text("Screen 2")
+        
+        ZStack {
+            Text("Screen 2!")
+            Text("Screen 2!")
+            Text("Screen 2!")
+            
+        }
     }
+    
 }
 
 #Preview {
