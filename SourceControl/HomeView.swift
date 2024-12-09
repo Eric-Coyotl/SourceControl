fed86186830e518432a900d53bd96149d4c98545
Eric <coyotleric@gmail.com>
2024-12-09 12:44:41 -0800

    [Feature] Delete extra text

--- SourceControl/HomeView.swift
+++ SourceControl/HomeView.swift
@@ -16,9 +16,6 @@ struct HomeView: View {
         VStack {
             Text("Hi")
             
-            Text("Screen 2!")
-            Text("Screen 2!")
-            Text("Screen 3!")
             
         }
         .onAppear{
