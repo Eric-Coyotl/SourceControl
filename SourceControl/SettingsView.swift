0356fe825fe05e23379caeae1492a649551e80a6
Eric <coyotleric@gmail.com>
2024-12-13 15:19:39 -0800

    [Feature] Add settings view

---
+++ SourceControl/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  SourceControl
+//
+//  Created by Eric Coyotl on 12/13/24.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Settings")
+    }
+}
+
+#Preview {
+    SettingsView()
+}
