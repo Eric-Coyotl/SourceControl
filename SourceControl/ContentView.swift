12caba6bcf687c67c42a8779082db838a7628930
Eric <coyotleric@gmail.com>
2024-12-13 15:32:46 -0800

    another commit

--- SourceControl/ContentView.swift
+++ SourceControl/ContentView.swift
@@ -49,7 +49,7 @@ struct ContentView: View {
                             .font(.largeTitle)
                             .imageScale(.large)
                             .foregroundStyle(.tint)
-                        Text("Swiftful Thinking!")
+                        Text("Swiftful Think!!!!!")
             
                         Button("Subscribe Now!"){
                         }
